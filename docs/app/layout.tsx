0e92733bae6f0cd6cc4febee4ace3cf27476cae1
Mateusz Porębski <mateusz.porebski99@gmail.com>
2026-06-23 11:46:29 +0200

    chore: update banner to announce 1.7.0 Enthusiast release (#413)

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 const banner = (
   <Banner storageKey="banner">
-    <a href="https://github.com/upsidelab/enthusiast/releases/tag/v1.6.0">Enthusiast 1.6.0 is released 🎉</a>
+    <a href="https://github.com/upsidelab/enthusiast/releases/tag/v1.7.0">Enthusiast 1.7.0 is released 🎉</a>
   </Banner>
 )
 
